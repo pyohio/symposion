29806a98e6d281b5bb8c28cf8804074c7cae15fd
Dave Forgac <tylerdave@tylerdave.com>
2018-02-25 02:27:44 -0500

    sort proposal sections by name

--- symposion/reviews/context_processors.py
+++ symposion/reviews/context_processors.py
@@ -3,7 +3,7 @@ from symposion.proposals.models import ProposalSection
 
 def reviews(request):
     sections = []
-    for section in ProposalSection.objects.all():
+    for section in ProposalSection.objects.all().order_by('section__name'):
         if request.user.has_perm("symposion_reviews.can_review_%s" % section.section.slug):
             sections.append(section)
     return {
