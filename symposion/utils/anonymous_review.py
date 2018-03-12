046004f618da4faaa2705af043f07e2e401668ae
Ben Freeman <bfreema8@kent.edu>
2018-03-11 22:32:22 -0400

    add additional notes & HTML to the redacted Proxy Proposal Getter

--- symposion/utils/anonymous_review.py
+++ symposion/utils/anonymous_review.py
@@ -7,17 +7,21 @@ class ProposalProxy(object):
     def __init__(self, proposal):
         self.__proposal__ = proposal
 
-    def __getattr__(self, attr):
-        ''' Overridden __getattr__ that hides the available speakers. '''
+     def __getattr__(self, attr):
+         ''' Overridden getattr to anonymize speaker data '''
 
-        if attr == "speaker":
-            return Parrot("Primary Speaker")
-        elif attr == "additional_speakers":
-            return None
-        elif attr == "speakers":
-            return self._speakers
-        else:
-            return getattr(self.__proposal__, attr)
+         if attr == "speaker":
+             return Parrot("Primary Speaker")
+         elif attr == "additional_speakers":
+             return None
+         elif attr == "speakers":
+             return self._speakers
+         elif attr == "additional_notes" :
+             return Parrot("Additional Notes")
+         elif attr == "additional_notes_html":
+             return Parrot("Additional Notes HTML")
+         else:
+             return getattr(self.__proposal__, attr)
 
     def _speakers(self):
         for i, j in enumerate(self.__proposal__.speakers()):
